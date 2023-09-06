244b138dccb5864cd4454d67ea2ffc666725b548
RaghavendraPatel <raghavpatel2808@hotmail.com>
2023-09-06 22:56:08 +0530

    updated cors options

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 const cors = require("cors");
 const corsOptions = {
   // origin: "http://localhost:3000",
-  origin: "https://placement-cell-raghavendra.netlify.app/signin",
+  origin: "https://placement-cell-raghavendra.netlify.app",
   credentials: true,
   optionSuccessStatus: 200,
 };
