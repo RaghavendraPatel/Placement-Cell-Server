86551cb7a3754bbdba0fce1d0f2b328f907299e7
RaghavendraPatel <raghavpatel2808@hotmail.com>
2023-09-07 21:57:48 +0530

    updated get request

--- controllers/job_controller.js
+++ controllers/job_controller.js
@@ -19,6 +19,7 @@ module.exports.getJobs = async function (req, res) {
 };
 
 module.exports.searchJobs = async function (req, res) {
+  console.log(req.body);
   const jobs = await axios
     .get(
       `http://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}&results_per_page=20&what=${req.body.what}&content-type=application/json`
