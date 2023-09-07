29f9fc1c1f839d95fec44e2ac6050c7032a26622
RaghavendraPatel <raghavpatel2808@hotmail.com>
2023-09-07 21:39:36 +0530

    updated get request

--- controllers/job_controller.js
+++ controllers/job_controller.js
@@ -1,12 +1,13 @@
 const dotenv = require("dotenv").config();
+const axios = require("axios");
 
 module.exports.getJobs = async function (req, res) {
-  const jobs = await fetch(
-    `http://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}&results_per_page=20&what=javascript%20developer&content-type=application/json`
-  )
-    .then((res) => res.json())
+  const jobs = await axios
+    .get(
+      `http://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}&results_per_page=20&what=javascript%20developer&content-type=application/json`
+    )
     .then((data) => {
-      return data.results;
+      return data.data.results;
     })
     .catch((err) => {
       console.log(err);
@@ -18,12 +19,12 @@ module.exports.getJobs = async function (req, res) {
 };
 
 module.exports.searchJobs = async function (req, res) {
-  const jobs = fetch(
-    `http://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}&results_per_page=20&what=${req.body.what}&content-type=application/json`
-  )
-    .then((res) => res.json())
+  const jobs = await axios
+    .get(
+      `http://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}&results_per_page=20&what=${req.body.what}&content-type=application/json`
+    )
     .then((data) => {
-      return data;
+      return data.data.results;
     })
     .catch((err) => {
       console.log(err);
